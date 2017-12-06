ea492eb29e19e2fef0269bc655288887a92c23c5
wilderliao <wilderliao@tencent.com>
2017-12-06 23:15:47 +0800

    更新例程

--- case_hello/hello_vue.js
+++ case_hello/hello_vue.js
@@ -4,3 +4,17 @@ var vue_obj = new Vue({
 		message:'Hello Vue!'
 	}
 })	
+
+var hover_obj = new Vue({
+	el:'#el_hover',
+	data:{
+		message:'页面加载于' + new Date().toLocaleString()
+	}
+})
+
+var seen_obj = new Vue({
+	el:'#el_seen',
+	data:{
+		seen:false
+	}
+})
