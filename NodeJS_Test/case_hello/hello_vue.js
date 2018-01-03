d880a4bf6b652ecc38357e70e6c8ddb8b5ba174c
wilderliao <wilderliao@tencent.com>
2018-01-03 21:02:43 +0800

    1

--- NodeJS_Test/case_hello/hello_vue.js
+++ NodeJS_Test/case_hello/hello_vue.js
@@ -11,10 +11,20 @@ var hover_obj = new Vue({
 		message:'页面加载于' + new Date().toLocaleString()
 	}
 })
-
 var seen_obj = new Vue({
 	el:'#el_seen',
 	data:{
 		seen:false
+	}
+})
+
+var todo_obj = new Vue({
+	el:'#el_todo',
+	data:{
+		todos:[
+			{text:'学习JavaScript'}，
+			{text:'学习 Vue'}，
+			{text:'学习 Node.js'}
+		]
 	}
 })
